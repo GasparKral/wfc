bc271f328eefde4643278f536441715c468eb7af
GasparKral <147488938+GasparKral@users.noreply.github.com>
2024-04-24 12:23:44 +0200

    multiThread

--- src/main/java/gaspardev/controller/MapViewController.java
+++ src/main/java/gaspardev/controller/MapViewController.java
@@ -9,12 +9,18 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Rotate;
-
+import javafx.application.Platform;
+import javafx.concurrent.Task;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import gaspardev.model.Cell;
 
 public class MapViewController {
 
     WaveFuntionColapse wfc;
+    private Task<Void> wfcTask;
+    private Task<Void> drawTask;
+    private BlockingQueue<Cell> updateQueue = new LinkedBlockingQueue<>();
 
     @FXML
     AnchorPane anchorPane;
@@ -28,6 +34,84 @@ public class MapViewController {
     @FXML
     public void setInitial(WaveFuntionColapse wfc) {
         this.wfc = wfc;
+    }
+
+    private void initWfcTask() {
+        wfcTask = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                wfc.clearEntropie();
+                wfc.fillEntropie();
+                wfc.rotations();
+
+                do {
+                    Cell tempCell = wfc.getCellWithMinimumEntropy().colapseTileR();
+                    tempCell.updateNeighborsValue();
+
+                    if (wfc.restartGrid()) {
+                        // Limpiar la cuadrícula y dibujar de nuevo
+                        Platform.runLater(() -> {
+                            grid.getChildren().clear();
+                            try {
+                                drawMap();
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
+                        });
+                        break; // Salir del bucle cuando se reinicia la cuadrícula
+                    }
+
+                    // Agregar a la cola de actualizaciones
+                    updateQueue.add(tempCell);
+
+                } while (!wfc.checkIsAllCollapsed());
+
+                return null;
+            }
+        };
+    }
+
+    private void initDrawTask() {
+        drawTask = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+
+                do {
+
+                    if (updateQueue.isEmpty()) {
+                        Thread.sleep(50);
+                    }
+
+                    Cell tempCell = updateQueue.take();
+                    // Obtener la imagen de la celda
+                    String imagePath = wfc.getTileDirRelative().split("resources")[1].concat("\\").replaceAll("\\\\",
+                            "/")
+                            + tempCell.getColapsedTile().getImg();
+                    Image image = new Image(getClass().getResourceAsStream(imagePath));
+                    ImageView imageView = new ImageView(image);
+
+                    imageView.getTransforms()
+                            .add(new Rotate(
+                                    tempCell.getColapsedTile().getRotation() * (360
+                                            / tempCell.getColapsedTile().getConexcions().length),
+                                    image.getWidth() / 2, image.getHeight() / 2));
+
+                    // Actualizar la interfaz de usuario en el hilo de JavaFX
+                    Platform.runLater(() -> {
+                        grid.add(imageView, tempCell.getPosX(), tempCell.getPosY());
+                    });
+
+                } while (!updateQueue.isEmpty());
+
+                return null;
+            }
+        };
+    }
+
+    private void executeTasks() {
+        // Ejecutar las tareas en hilos separados
+        new Thread(wfcTask).start();
+        new Thread(drawTask).start();
     }
 
     @FXML
@@ -54,45 +138,9 @@ public class MapViewController {
         scrollPane.setContent(grid);
 
         // Draw
-        draw();
-
-    }
-
-    private void draw() {
-
-        // Limpiar el y setear el estado base
-
-        wfc.clearEntropie();
-        wfc.fillEntropie();
-        wfc.rotations();
-
-        do {
-
-            // Obtener la celda con menor entropia, colapsarla con el mejor valor y
-            // actualizar los vecinos
-            Cell tempCell = wfc.getCellWithMinimumEntropy().colapseTileR();
-            tempCell.updateNeighborsValue();
-
-            // Obtener la imagen de la celda
-            String imagePath = wfc.getTileDirRelative().split("resources")[1].concat("\\").replaceAll("\\\\", "/")
-                    + tempCell.getColapsedTile().getImg();
-            Image image = new Image(getClass().getResourceAsStream(imagePath));
-            ImageView imageView = new ImageView(image);
-
-            imageView.getTransforms()
-                    .add(new Rotate(
-                            tempCell.getColapsedTile().getRotation() * (360
-                                    / tempCell.getColapsedTile().getConexcions().length),
-                            image.getWidth() / 2, image.getHeight() / 2));
-
-            grid.add(imageView, tempCell.getPosX(), tempCell.getPosY());
-
-            if (wfc.restartGrid()) {
-                draw();
-            }
-
-        } while (!wfc.checkIsAllCollapsed());
-
+        initWfcTask();
+        initDrawTask();
+        executeTasks();
     }
 
 }
